b56662ab22139f13639f546259ee8977850fc000
gabbypierce <144390401+gabbypierce@users.noreply.github.com>
2024-04-05 22:47:02 -0400

    Add files via upload

---
+++ src/main/java/com/hfad/guessinggame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hfad.guessinggame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.quinnipiac.ser210.ls06_network_nav.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
